faf7d2caca6a0601f04b65610a221c0c5044a127
Mahmoud Ali <ahmedma722m@gmail.com>
2025-12-29 21:29:28 +0200

    Initial project structure

---
+++ Ecom.Core/Entity/Product/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.Entity.Product
+{
+    public class Product: BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public virtual Category Category { get; set; } = new Category();
+        public virtual List<Photo> Photos { get; set; }
+    }
+}
